94be37c153244f0c998043e29a474b1419a0fd1f
Sopan43 <sopanmittal43@gmail.com>
2018-01-11 15:18:13 +0530

    Add login

--- app.js
+++ app.js
@@ -55,13 +55,51 @@ app.get('/users', function(req, res) {
 
 /************************************************************************************************************************************
  *																																	*
+ *													Get USER Login																	*
+ *																																	*
+ ************************************************************************************************************************************/
+
+app.get('/login', function(req, res) {
+	var queryParams = req.query;
+	var usersEmail = [];
+	return new Promise(function(resolve, rej) {
+
+		if (queryParams.hasOwnProperty('email') && queryParams.email.trim().length > 0 &&
+			queryParams.hasOwnProperty('pass') && queryParams.pass.trim().length > 0) {
+
+			con.query('SELECT email,password FROM users WHERE email = ?', [queryParams.email],(err, rows,fields) => {
+				if (err) throw err;
+				
+				if (passwordHash.verify(queryParams.pass, rows[0].password)) {
+					resolve('User login seccessful');
+				} else {
+						rej(404);
+				}
+
+			});
+
+		}
+	}).then(function(data) {
+		
+		return res.json('User Login seccessful');
+	}, function(error) {
+		
+		return res.status(error).send();
+	});
+
+});
+
+
+/************************************************************************************************************************************
+ *																																	*
  *													POST SignUp Users																*
  *																																	*
  ************************************************************************************************************************************/
 
-app.post('/users', function(req, res) {
+app.post('/register', function(req, res) {
 	var body = _.pick(req.body, 'email', 'password', 'name', 'city', 'country', 'lon', 'lat');
-	var matchedEmail
+	//	var matchedEmail;
+	var objEx = 10;
 
 	_.defaults(body, {
 		city: 'NA',
@@ -73,7 +111,7 @@ app.post('/users', function(req, res) {
 	if (body.email.trim().length === 0 || body.password.trim().length === 0 || body.name.trim().length === 0) {
 		return res.status(400).send();
 	}
-	var hashedPassword = passwordHash.generate(body.password.trim());
+	var hashedPassword = body.password.trim();// = passwordHash.generate(body.password.trim());
 
 	body.email = body.email.trim();
 	body.password = hashedPassword;
@@ -81,18 +119,36 @@ app.post('/users', function(req, res) {
 
 	var findEmail = body.email;
 
-	con.query('SELECT email FROM users WHERE email = ?', [findEmail], (err, rows) => {
-		if (err) throw err;
-		if (rows.length > 0) {
-			return res.status(406).json('Email already exist');
-		} else {
-			con.query('INSERT INTO users SET ?', body, (err, res) => {
-				if (err) throw (err)
-			});
-			return res.json(body);
 
-		}
+	return new Promise(function(resolve, rej) {
+
+		con.query('SELECT email FROM users WHERE email = ?', [findEmail], (err, rows) => {
+			if (err) throw err;
+			if (rows.length > 0) {
+				return res.status(406).json('Email already exist');
+			} else {
+				objEx = 20 //{dsadsad :'asdasd', adsadaddd:'dasdasd'};
+
+				con.query('INSERT INTO users SET ?', body, (err, res) => {
+					if (err) throw (err)
+				});
+
+				resolve(objEx);
+
+				//return res.json(body);
+
+			}
+
+		});
+	}).then(function(data) {
+		console.log(objEx);
+		console.log('Resolve: ' + data);
+		return res.json(body);
+	}, function(error) {
+		console.log('Reject: ' + error);
 	});
+
+
 });
 
 
