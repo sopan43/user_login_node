fd515b86a4ea5d626143c1eeec0dfc5acd143d5a
Sopan43 <sopanmittal43@gmail.com>
2018-01-11 11:55:49 +0530

    Unique eamil register

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var _ = require('underscore');
+var passwordHash = require('password-hash');
+var mysql = require('mysql');
 var users = [];
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -14,13 +16,40 @@ app.get('/', function(req, res) {
 
 /************************************************************************************************************************************
  *																																	*
- *													Get All Todos Items																*
+ *													My sql Connection																*
+ *																																	*
+ ************************************************************************************************************************************/
+
+var con = mysql.createConnection({
+	host: "localhost",
+	user: "root",
+	password: "root123",
+	database: "Users"
+});
+
+con.connect(function(err) {
+	if (err) throw err;
+	console.log("Connected!");
+});
+
+/************************************************************************************************************************************
+ *																																	*
+ *													Get All Users																	*
  *																																	*
  ************************************************************************************************************************************/
 
 app.get('/users', function(req, res) {
 
-	res.json(users);
+	var usersEmail = [];
+
+	con.query('SELECT email,name FROM users', (err, rows) => {
+		if (err) throw err;
+		console.log('Data received from Db:\n');
+		usersEmail = (rows);
+		console.log(usersEmail);
+		res.json(usersEmail);
+	});
+
 });
 
 
@@ -31,18 +60,39 @@ app.get('/users', function(req, res) {
  ************************************************************************************************************************************/
 
 app.post('/users', function(req, res) {
-	var body = _.pick(req.body, 'email', 'password', 'name', 'city', 'country', 'long', 'lat');
+	var body = _.pick(req.body, 'email', 'password', 'name', 'city', 'country', 'lon', 'lat');
+	var matchedEmail
+
+	_.defaults(body, {
+		city: 'NA',
+		country: 'NA',
+		lon: 'NA',
+		lat: 'NA'
+	});
 
 	if (body.email.trim().length === 0 || body.password.trim().length === 0 || body.name.trim().length === 0) {
 		return res.status(400).send();
 	}
+	var hashedPassword = passwordHash.generate(body.password.trim());
+
 	body.email = body.email.trim();
-	body.password = body.password.trim();
+	body.password = hashedPassword;
 	body.name = body.name.trim();
 
-	users.push(body);
+	var findEmail = body.email;
 
-	res.json(body);
+	con.query('SELECT email FROM users WHERE email = ?', [findEmail], (err, rows) => {
+		if (err) throw err;
+		if (rows.length > 0) {
+			return res.status(406).json('Email already exist');
+		} else {
+			con.query('INSERT INTO users SET ?', body, (err, res) => {
+				if (err) throw (err)
+			});
+			return res.json(body);
+
+		}
+	});
 });
 
 
